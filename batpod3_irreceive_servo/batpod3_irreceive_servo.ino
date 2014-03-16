5692964e6150f44a78b505fedfa7412435e06c13
LeSanglier <lesanglierdesardennes@gmail.com>
2014-03-16 12:27:10 +0100

    Ajout bras et commande

--- batpod3_irreceive_servo/batpod3_irreceive_servo.ino
+++ batpod3_irreceive_servo/batpod3_irreceive_servo.ino
@@ -10,6 +10,12 @@ Arduino Uno
 
 // Servo
 Servo ServoGauche;
+Servo servoD5; 
+Servo servoD6;                
+ 
+int posServoD5 = 0;   
+int posServoD6 = 0;
+
 
 int posServoGauche = 180;
 
@@ -26,7 +32,9 @@ void setup()
     Serial.println("Init Servo"); 
     ServoGauche.attach(7);
     ServoGauche.write(posServoGauche); 
-    
+    servoD5.attach(5);
+    servoD6.attach(6); 
+  
     Serial.println("Init IR"); 
     Serial.begin(38400);
     IR.Init(A3);
@@ -43,7 +51,57 @@ void loop() {
           byteRead = Serial.read();
           
           switch (byteRead) {
-           
+
+ 
+             // Servo D5
+            case 119: { // w
+                    Serial.println("Serie : ServoD5 baisser");
+                        if (posServoD5 > 20) {
+                              posServoD5 -= 20;
+                              servoD5.write(posServoD5);
+                              Serial.print("Serie : posServoD5 :" );
+                              Serial.println(posServoD5);
+                              delay(15);
+                        }
+                        break;
+                      }
+            case 120: { // x
+                    Serial.println("Serie : ServoD5 lever");
+                        if (posServoD5 < 160) {
+                              posServoD5 += 20;
+                               servoD5.write(posServoD5);  
+                               Serial.print("Serie : posServoD5 :" );
+                              Serial.println(posServoD5);                           
+                              delay(15);
+                        }
+                        break;
+                      }
+  
+             // Servo D6                    
+             case 99: { // c
+                    Serial.println("Serie : ServoD6 baisser");
+                        if (posServoD6 > 20) {
+                              posServoD6 -= 20;
+                              servoD6.write(posServoD6);
+                              Serial.print("Serie : posServoD6 :" );
+                              Serial.println(posServoD6);
+                              delay(15);
+                        }
+                        break;
+                      }
+            case 118: { // v
+                    Serial.println("Serie : ServoD6 lever");
+                        if (posServoD6 < 160) {
+                              posServoD6 += 20;
+                               servoD6.write(posServoD6);  
+                               Serial.print("Serie : posServoD6 :" );
+                              Serial.println(posServoD6);                           
+                              delay(15);
+                        }
+                        break;
+                      }                     
+
+            
             case 113: { // q
                         Serial.println("Serie : Servo Gauche Avant");                      
                         for(posServoGauche = 0; posServoGauche < 180; posServoGauche += 1){
